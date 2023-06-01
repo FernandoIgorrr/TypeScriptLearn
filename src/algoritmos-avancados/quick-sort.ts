aea2a849cede71250c949e9a3e039ab2b7d0d3af
Fernando Igor <fernando_id.7@hotmail.com>
2023-06-01 03:00:31 -0300

    ...

---
+++ src/algoritmos-avancados/quick-sort.ts
@@ -0,0 +1,13 @@
+
+function isEven(n: number){
+    return n%2 === 0;
+}
+
+function isOdd(n: number){
+    return !isEven(n);
+}
+
+function quickSort(array: number[]){
+    let pivot: number = array[0];
+
+}
